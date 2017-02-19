21c552094628e8408ec8203d6db465f395ef8bfd
Tim Beurskens <timbeurskens.97@gmail.com>
2017-02-19 17:07:27 +0100

    create testcases

---
+++ test/windowevents-test.js
@@ -0,0 +1,90 @@
+/**
+ * Created by s154796 on 19-2-2017.
+ */
+var assert = require('assert');
+var windowEvents = require('../src/windowevents');
+
+suite('Array', function () {
+    setup(function () {
+
+    });
+
+    suite('#indexOf', function () {
+        test('should return -1 when not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        test('should return a value >= 0 when present', function () {
+            var result = [1, 2, 3, 4].indexOf(4);
+            assert(result >= 0);
+        });
+        test('should return the first index of a present instance', function () {
+            assert.equal(1, [1, 2, 1, 2].indexOf(2));
+            assert.equal(0, [1, 2, 1, 2].indexOf(1));
+        });
+    });
+});
+
+suite("WindowEvents", function () {
+    setup(function () {
+
+    });
+
+    suite("EventTransmitter", function () {
+        test('should throw an error on an invalid window object', function () {
+            assert.throws(function(){
+                new windowEvents.EventTransmitter({});
+            }, Error);
+        });
+        test('should return an object when a valid window object is given', function () {
+            var transmitter = new windowEvents.EventTransmitter(window);
+            assert(typeof transmitter == "object");
+        });
+    });
+
+    suite("EventListener", function () {
+        test('should fire an event when transmitting', function (done) {
+            windowEvents.clearListeners();
+            windowEvents.listen("test1", function () {
+                done();
+            });
+            windowEvents.send("test1", null);
+        });
+
+        test('should listen for default events', function (done) {
+            windowEvents.clearListeners();
+            windowEvents.listen(null, function () {
+                done();
+            });
+
+            windowEvents.send(null, null);
+        });
+
+        test('should listen for undefined events', function (done) {
+            windowEvents.clearListeners();
+            windowEvents.listen(null, function () {
+                done();
+            });
+
+            windowEvents.send("test", null);
+        });
+
+        test('should return false when adding duplicate listeners', function () {
+            windowEvents.clearListeners();
+            var listener = function () {
+
+            };
+            assert.equal(windowEvents.listen("test", listener), true, "first instance");
+            assert.equal(windowEvents.listen("test", listener), false, "second instance");
+        });
+
+        test('should be able to remove existing listeners', function () {
+            windowEvents.clearListeners();
+            var listener = function () {
+
+            };
+            windowEvents.listen("test", listener);
+            assert.equal(windowEvents.removeListener("test", listener), true, "first instance");
+            assert.equal(windowEvents.removeListener("test", listener), false, "second instance");
+        });
+    });
+});
