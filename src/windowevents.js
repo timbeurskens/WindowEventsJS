4ae0fb611c971677a3e28def839f1ddc17a0d9c8
Tim Beurskens <timbeurskens.97@gmail.com>
2017-02-19 17:08:10 +0100

    implement basic functionality

--- src/windowevents.js
+++ src/windowevents.js
@@ -1,9 +1,87 @@
 /**
  * Created by s154796 on 19-2-2017.
  */
+var eventListeners = {};
 
-function windowEvents() {
+function receiveMessage(event) {
+    var eventData = event.data['data'] || {};
+    var eventName = event.data['eventName'] || null;
 
+    var origin = event.origin;
+    var source = event.source;
+
+    var targetListeners = eventListeners[eventName] || eventListeners[null] || [];
+    var replyTransmitter;
+
+    try {
+        replyTransmitter = new windowEvents.EventTransmitter(source);
+    }catch (e){
+        replyTransmitter = null;
+    }
+
+    targetListeners.forEach(function (listener) {
+        listener(eventData, replyTransmitter, origin);
+    });
 }
+
+function postMessage(window, eventName, data) {
+    window.postMessage({eventName: eventName, data: data}, "*");
+}
+
+var windowEvents = {
+    listen: function (eventName, callback) {
+        if(eventListeners[eventName] === undefined){
+            eventListeners[eventName] = [];
+        }
+
+        if(typeof callback == "function" && eventListeners[eventName].indexOf(callback) < 0){
+            eventListeners[eventName].push(callback);
+            return true;
+        }
+
+        return false;
+    },
+
+    removeListener: function (eventName, callback) {
+        if(eventListeners[eventName] === undefined || typeof callback != "function"){
+            return false;
+        }
+
+        var index = eventListeners[eventName].indexOf(callback);
+        if(index >= 0){
+            eventListeners[eventName].splice(index, 1);
+            return true;
+        }else {
+            return false;
+        }
+    },
+
+    EventTransmitter: function (window) {
+        if(typeof window.postMessage == "function"){
+            return {
+                send: function (eventName, data) {
+                    postMessage(window, eventName, data);
+                }
+            };
+        }else {
+            throw new Error("window.postMessage is undefined, is the window parameter a real Window object?");
+        }
+    },
+
+    transmitterFromIframe: function (frame) {
+        var w = frame.contentWindow || frame;
+        return new this.EventTransmitter(w);
+    },
+
+    send: function (eventName, data) {
+        postMessage(window, eventName, data);
+    },
+
+    clearListeners: function () {
+        eventListeners = {};
+    }
+};
+
+window.addEventListener("message", receiveMessage, false);
 
 module.exports = windowEvents;
